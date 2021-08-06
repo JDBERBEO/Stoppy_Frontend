9106d67a26e81cbf6361ba7a6acf17c7f2e0c4ec
Juan <jd.berbeo@gmail.com>
2021-08-06 16:17:12 -0500

    crea imagen logo de stoppy

--- App.js
+++ App.js
@@ -1,14 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { ImageBackground, StyleSheet, Text, View } from 'react-native';
+import { ImageBackground, StyleSheet, Text, View, Button, TextInput, Image } from 'react-native';
 
 export default function App() {
-  
+  const [text, onChangeText] = React.useState("")
+
   return (
     <View style={styles.container}>
       <ImageBackground source={require("./assets/paper1.jpeg")} resizeMode="cover" style={styles.image}>
-      <Text style={styles.text}>Welcome to stoppy!</Text>
-
+        <Image source={require("./assets/Stoppy_big_Logo-NoBk.png")} style={styles.imageLogo} resizeMode="cover" />
+        <Text style={styles.text}>Welcome to stoppy!</Text>
+        <TextInput style={styles.input} onChangeText={onChangeText} value={text}/>
+        <Button title="Ingresa" />
       </ImageBackground>
       <StatusBar style="auto" />
     </View>
@@ -23,7 +26,24 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent:"center"
   },
+  imageLogo: {
+    flex:1,
+    alignItems: "center",
+    justifyContent: "center",
+
+    width: "100%",
+    height: "60%"
+
+  },
   text:{
     textAlign:"center"
-  }
+  },
+  input: {
+    height: 40,
+    marginHorizontal: 120,
+    borderBottomColor: "blue",
+    borderBottomWidth: 1,
+    borderWidth: 0,
+    padding: 10,
+  },
 });
